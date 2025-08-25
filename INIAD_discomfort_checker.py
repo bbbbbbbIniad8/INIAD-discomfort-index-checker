f52e43757c22dbdc38fa983fe2aa12a10877e957
0x7b <0x7b123@gmail.com>
2025-08-25 22:06:32 +0900

    コードの整形

--- INIAD_discomfort_checker.py
+++ INIAD_discomfort_checker.py
@@ -15,6 +15,7 @@ def get_info(id, password, room):
 def discomfort_index(temperature, humidity):
     return 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
 
+
 def display_result(data):
     try:
         room_num = data[1]['room_num']
@@ -33,4 +34,3 @@ def display_result(data):
             print("外部ネットワークではサービスを利用することはできません。")
         else:
             print(e)
-
