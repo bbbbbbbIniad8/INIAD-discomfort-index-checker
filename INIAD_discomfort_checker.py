1fbab10fe623170e67ab4a65db563dec83de8c42
0x7b <0x7b123@gmail.com>
2025-08-25 22:08:17 +0900

    コードの修正

--- INIAD_discomfort_checker.py
+++ INIAD_discomfort_checker.py
@@ -12,6 +12,7 @@ def get_info(id, password, room):
     except Exception as e:
         print({"status": "error", "description": str(e)})
 
+
 def discomfort_index(temperature, humidity):
     return 0.81 * temperature + 0.01 * humidity * (0.99 * temperature - 14.3) + 46.3
 
